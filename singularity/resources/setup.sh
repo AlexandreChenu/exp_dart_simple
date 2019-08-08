d1f910a70678a693c159dd725fd0a5dffd0c78eb
Alexandre Chenu <38214810+AlexandreChenu@users.noreply.github.com>
2019-08-08 11:37:16 +0100

    fixed typo

--- singularity/resources/setup.sh
+++ singularity/resources/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./waf configure --exp example_dart_simple --dart /workspace --kdtree /workspace/include --robot_dart /workspace
+./waf configure --exp exp_dart_simple --dart /workspace --kdtree /workspace/include --robot_dart /workspace
 ./waf --exp exp_dart_simple
