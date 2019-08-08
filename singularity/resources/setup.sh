40696dcafd6807e5e7d2155d3b79df34d5a66d4d
Alexandre Chenu <38214810+AlexandreChenu@users.noreply.github.com>
2019-08-08 11:21:57 +0100

    fixed setup

--- singularity/resources/setup.sh
+++ singularity/resources/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./waf configure --exp example_dart_exp --dart /workspace --kdtree /workspace/include --robot_dart /workspace
-./waf --exp exp_dart_test
+./waf configure --exp example_dart_simple --dart /workspace --kdtree /workspace/include --robot_dart /workspace
+./waf --exp exp_dart_simple
